2a37b1fa4fba25f7cc54e37c4b404d60df465ffe
mchapp64 <michael.c.chappell@gmail.com>
2014-07-12 12:20:11 -0400

    removed border around legend in third plot

--- plot4.R
+++ plot4.R
@@ -29,7 +29,7 @@ with(df2, plot(DT, Voltage, type="l", ylab = "Voltage", xlab = "datetime"))
 with(df2, plot(DT, Sub_metering_1, type="l", ylab = "Energy sub metering", xlab = "", col = "black"))
 with(df2, lines(DT, Sub_metering_2, col = "red"))
 with(df2, lines(DT, Sub_metering_3, col = "blue"))
-legend("topright", lty = c(1,1,1), col = c("black", "red", "blue"), legend = c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"))
+legend("topright", lty = c(1,1,1), bty = "n", col = c("black", "red", "blue"), legend = c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"))
 }
 
 with(df2, plot(DT, Global_reactive_power, type="l", ylab = "Global reative power", xlab = "datetime"))
